9b48b4c48aa12c8111725f1db8862aac9cb775b2
Abril Mosquera <lagartosautos@gmail.com>
2024-05-15 12:08:36 -0300

    Separe las rutas en archivos, y cree el css,falta el blog ,ejercicio 4

--- peliculas/__init__.py
+++ peliculas/__init__.py
@@ -1,39 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 with app.app_context():
     from . import db
     db.init_app(app)
+    
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+from . import lenguajes
+app.register_blueprint(lenguajes.bp)
 
-@app.route("/lenguaje")
-def lenguaje():
-    consulta = """
-        SELECT name FROM language
-        ORDER BY name;
-    """
-    con = db.get_db()
-    res = con.execute(consulta)
-    lista_lenguaje = res.fetchall()
-    pagina = render_template("language.html",
-                             lenguajes = lista_lenguaje)
 
-    return pagina
-
-@app.route("/category")
-def category():
-    consulta = """
-        SELECT name FROM category
-        ORDER BY name;
-    """
-    con = db.get_db()
-    res = con.execute(consulta)
-    lista_category = res.fetchall()
-    pagina = render_template("category.html", categoria = lista_category)
-    
-    return pagina
+from . import categoria
+app.register_blueprint(categoria.bp)
